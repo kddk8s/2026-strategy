d3e118b0cf2515e17d03a2460f8d268802edf854
Katie Eris <134787536+kddk8s@users.noreply.github.com>
2026-08-30 14:49:25 -0400

    Added another fun data source

--- bin/fetch-wake-gov.sh
+++ bin/fetch-wake-gov.sh
@@ -11,10 +11,12 @@ Main(){
 }
 Files(){
 cat << EEOOTT
-https://github.com/kddk8s/2026-strategy.git
 https://services.wake.gov/realdata_extracts/2026_Residential_Report.xlsx
 https://services.wake.gov/realdata_extracts/PhyAddr_TabDelmTxtFile.txt
 https://services.wake.gov/realdata_extracts/RealEstData08292026.xlsx
+https://services.wake.gov/collection_extracts/REAL_ESTATE_FULL853_08282026.XLSX
+https://services.wake.gov/collection_extracts/REAL_ESTATE_delq853_08282026.xlsx
+https://s3.us-west-1.amazonaws.com/wakegov.com.if-us-west-1/s3fs-public/documents/2026-08/June%202026%20Permit%20Report.xlsx
 EEOOTT
 }
 # shellcheck disable=SC2317 # Typical pattern to make safe on NFS
