58ddfda9ef65295b2f8638e9412d785cf82aa1cb
Katie Eris <134787536+kddk8s@users.noreply.github.com>
2026-08-31 11:44:37 -0400

    Just mirror the wake gov stuff because names change each day

--- bin/fetch-wake-gov.sh
+++ bin/fetch-wake-gov.sh
@@ -5,20 +5,20 @@ zero=$(readlink -f "$0")
 zerodir="${zero%/*}"
 mirrordir="${zerodir}/../web-mirror/wake.gov"
 Main(){
-    wget --timestamping --force-directories --no-verbose --input-file=<(Files) --directory-prefix="$mirrordir" --compress=gzip
+    rc=0
+    wget --timestamping --force-directories --no-verbose --input-file=<(Files) --directory-prefix="$mirrordir" --compress=gzip --level=1 --no-parent --recursive --execute robots=off || rc="$?"
+    [ "$rc" == 0 ] || [ "$rc" == 8 ]
     
     exit 0
 }
 Files(){
 cat << EEOOTT
-https://services.wake.gov/realdata_extracts/2026_Residential_Report.xlsx
-https://services.wake.gov/realdata_extracts/PhyAddr_TabDelmTxtFile.txt
-https://services.wake.gov/realdata_extracts/RealEstData08292026.xlsx
-https://services.wake.gov/collection_extracts/REAL_ESTATE_FULL853_08282026.XLSX
-https://services.wake.gov/collection_extracts/REAL_ESTATE_delq853_08282026.xlsx
+https://services.wake.gov/realdata_extracts/
+https://services.wake.gov/collection_extracts/
 https://s3.us-west-1.amazonaws.com/wakegov.com.if-us-west-1/s3fs-public/documents/2026-08/June%202026%20Permit%20Report.xlsx
 EEOOTT
 }
+#$ wget --quiet --recursive --level=1 --no-parent --directory-prefix=approved-capture https://archive.example.net/exports/
 # shellcheck disable=SC2317 # Typical pattern to make safe on NFS
 Main "$@"
 exit 1
